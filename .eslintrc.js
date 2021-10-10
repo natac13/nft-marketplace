379ffc3cf27418586ad17992258c3c74fddda2af
Sean Campbell <sean.campbell13@gmail.com>
2021-10-10 15:40:24 -0400

    feat: create market place contract with small test

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,29 +6,31 @@ module.exports = {
     node: true,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
+    'standard',
+    // 'plugin:prettier/recommended',
+    'prettier',
+    'plugin:node/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    "node/no-unsupported-features/es-syntax": [
-      "error",
-      { ignores: ["modules"] },
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { ignores: ['modules'] },
     ],
+    'no-unused-vars': 'off',
     // note you must disable the base rule as it can report incorrect errors
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
   },
-};
+}
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,55 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe('NFTMarket', function () {
+  it('Should create and execute market sales', async function () {
+    const Market = await ethers.getContractFactory('NFTMarket')
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const NFT = await ethers.getContractFactory('NFT')
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const listingPrice = await (await market.getListingPrice()).toString()
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const auctionPrice = ethers.utils.parseUnits('10', 'ether')
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+    await nft.createToken('https://www.mytokenlocation.com')
+    await nft.createToken('https://www.mytokenlocation2.com')
+
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, {
+      value: listingPrice,
+    })
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, {
+      value: listingPrice,
+    })
+
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    await market
+      .connect(buyerAddress)
+      .createMarketSale(nftContractAddress, 1, { value: auctionPrice })
+
+    const items = await market.fetchMarketItems()
+
+    const humandReadableItems = await Promise.all(
+      items.map(async (item) => {
+        const tokenUri = await nft.tokenURI(item.tokenId)
+        const newItem = {
+          price: item.price.toString(),
+          tokenId: item.tokenId.toString(),
+          seller: item.seller,
+          owner: item.owner,
+          tokenUri,
+        }
+        return newItem
+      })
+    )
+    console.log('itemes: ', humandReadableItems)
+
+    expect(items.length).to.equal(1)
+  })
+})
